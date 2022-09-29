2470c5b7bffee1fba13370f2349f279ff114cee8
Mack Grissom <mcintyregrissom@gmail.com>
2022-09-29 12:47:40 +0700

    added loader and error componets

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,3 +1,9 @@
+import {link} from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import PlayPause from './PlayPause';
+import { playPause, setActiveSongs } from '../redux/features/playerSlice';
+
+
 const SongCard = () => (
   <div>SongCard</div>
 );
