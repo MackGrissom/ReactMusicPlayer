fe9934950ebeeb0bcffe9ca349ddbf36649421c1
Mack Grissom <mcintyregrissom@gmail.com>
2022-09-29 14:19:45 +0700

    added song card and discover logic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { shazamCoreApi } from './Services/shazamCore';
+import { shazamCoreApi } from './services/shazamCore';
 import playerReducer from './features/playerSlice';
 
 export const store = configureStore({
